d2406207ef83e7d1bd3fa86b465e72844c1afcec
samzevedo <samuel.azevedo@live.com>
2022-10-05 20:36:13 -0300

    add door textures

--- src/js/main.js
+++ src/js/main.js
@@ -63,14 +63,17 @@ window.addEventListener('dblclick', () => {
     }
 })
 
+// TEXTURES
+const textureLoader = new THREE.TextureLoader()
+
 //LIGHTS
 // AMBIENT LIGHT
-const ambientLight = new THREE.AmbientLight('#FFFFFF', 0.5)
+const ambientLight = new THREE.AmbientLight('#5697FE', 0.12)
 scene.add(ambientLight)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 
 // DIRECTIONAL LIGHT
-const moonLight = new THREE.DirectionalLight('#ffffff', 0.5)
+const moonLight = new THREE.DirectionalLight('#b9d5ff', 0.12)
 moonLight.position.set(4, 5, -2)
 scene.add(moonLight)
 gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
@@ -108,14 +111,52 @@ roof.rotation.y = Math.PI * 0.25
 roof.position.y = 3
 house.add(roof)
 
+// DOOR TEXTURE
+const doorColorTexture = textureLoader.load('../static/textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('../static/textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load(
+    '../static/textures/door/ambientOcclusion.jpg'
+)
+const doorMeltalnessTexture = textureLoader.load(
+    '../static/textures/door/metalness.jpg'
+)
+const doorRoughnessTexture = textureLoader.load(
+    '../static/textures/door/roughness.jpg'
+)
+const doorHeightTexture = textureLoader.load(
+    '../static/textures/door/height.jpg'
+)
+const doorNormalTexture = textureLoader.load(
+    '../static/textures/door/normal.jpg'
+)
 // DOOR
 const door = new THREE.Mesh(
-    new THREE.PlaneGeometry(2, 2),
-    new THREE.MeshStandardMaterial({ color: '#aa7b7b' })
+    new THREE.PlaneGeometry(2.2, 2.2, 100, 100),
+    new THREE.MeshStandardMaterial({
+        map: doorColorTexture,
+        alphaMap: doorAlphaTexture,
+        aoMap: doorAmbientOcclusionTexture,
+        displacementMap: doorHeightTexture,
+        displacementScale: 0.1,
+        normalMap: doorNormalTexture,
+        metalnessMap: doorMeltalnessTexture,
+        roughnessMap: doorRoughnessTexture,
+        transparent: true,
+    })
+)
+
+door.geometry.setAttribute(
+    'uv2',
+    new THREE.Float32BufferAttribute(door.geometry.attributes.uv.array, 2)
 )
 door.position.y = 1
-door.position.z = 2 + 0.01
+door.position.z = 2 + 0.00001
 house.add(door)
+
+// DOOR LIGHT
+const doorLight = new THREE.PointLight('#ff7d46', 1, 7)
+doorLight.position.set(0, 2.2, 2.7)
+house.add(doorLight)
 
 // BUSHES
 const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
@@ -136,6 +177,33 @@ bush4.position.set(-1.5, 0.2, 2.3)
 
 house.add(bush1, bush2, bush3, bush4)
 
+// GRAVES
+const graves = new THREE.Group()
+scene.add(graves)
+
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial({ color: '#AEAEAE' })
+
+for (let i = 0; i < 40; i++) {
+    const angle = Math.random() * Math.PI * 2 // random angles
+    const radius = 3 + Math.random() * 6.5 // random radius
+    const x = Math.cos(angle) * radius // get the x position using cosinus
+    const z = Math.sin(angle) * radius // get the z position using sinus
+
+    // create the mesh
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+    // position
+    grave.position.set(x, 0.3, z)
+    grave.rotation.y = (Math.random() - 0.5) * 0.4
+    grave.rotation.z = (Math.random() - 0.5) * 0.4
+
+    graves.add(grave)
+}
+
+// FOG
+const fog = new THREE.Fog('#464647', 5, 18)
+scene.fog = fog
+
 // RENDERER
 const renderer = new THREE.WebGL1Renderer({
     canvas,
@@ -143,6 +211,7 @@ const renderer = new THREE.WebGL1Renderer({
 
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor('#464647')
 
 // Animation
 const looping = () => {
